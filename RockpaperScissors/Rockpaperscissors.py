06287adbd21da729f2c61a8b5c9c47e56b4cf9e6
Soumya Somasundaran <14910227+soumyas567@users.noreply.github.com>
2021-07-13 12:08:35 +0100

    Update Rockpaperscissors.py

--- RockpaperScissors/Rockpaperscissors.py
+++ RockpaperScissors/Rockpaperscissors.py
@@ -1,1 +1,31 @@
 
+import random
+
+# list of choice
+
+choice = ["rock", 'paper', "scissor"]
+
+computer = random.choice(choice)
+user = False
+
+
+def rps_input():
+    user = input("Enter r | p | s")
+    if user == computer:
+        return "Tie"
+
+
+def rps(player1, player2):
+    rps_input()
+    if (player1 == 'r' and player2 == 's'):
+        return 'Rock crushes scissor!'
+    elif player1 == 's' and player2 == 'p':
+        return 'Scissor cut paper!'
+    elif player1 == 'p' and player2 == 'r':
+        return "Paper covers rock"
+    else:
+        return 'You fail'
+
+
+print(rps(user, computer))
+print(computer)
