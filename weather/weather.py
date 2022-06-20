d6765eb1abaef22aae3902487fc1c43089e43ca2
soumyasomasundaran <14910227+soumyas567@users.noreply.github.com>
2022-06-20 10:57:46 +0100

    units updated

--- weather/weather.py
+++ weather/weather.py
@@ -1,24 +1,16 @@
 import streamlit as st
-<<<<<<< HEAD
 import requests
-API_KEY = "XXXXXXXXXXXXXXXXX"
-def convert_to_celcius(temperature_in_kelvin):
-    return temperature_in_kelvin -273.15
-=======
-API_KEY = ''
-DEGREE_SIGN = u'\N{DEGREE SIGN}'
-
->>>>>>> 7673abf6844f6605cc945da9fb080124f3c77376
+API_KEY = "XXXXXXX"
 
 
 
 def find_current_weather(city):
-    base_url  = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    base_url  = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     weather_data = requests.get(base_url).json()
     try:
         general = weather_data['weather'][0]['main']
         icon_id = weather_data['weather'][0]['icon']
-        temperature = round(convert_to_celcius(weather_data['main']['temp']))
+        temperature = round(weather_data['main']['temp'])
         icon = f"http://openweathermap.org/img/wn/{icon_id}@2x.png"
     except KeyError:
         st.error("City Not Found")
@@ -41,13 +33,6 @@ def main():
             st.write(general)
             st.image(icon)
     
-
-
-
-
-
-
-
 
 
 
